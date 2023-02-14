e08c8202bc0250ef78aa3ba551009a5044098701
Antonio Walber <antoniowalber3000@hotmail.com.br>
2023-02-13 23:02:20 -0300

    Explicando detalhes por detalhes das funcionalidades

--- DataReceiver.php
+++ DataReceiver.php
@@ -1,46 +1,86 @@
 <?php
 include_once "Database.php";
+
+/**
+ * Classe que recebe os dados de captura de eventos do mouse e imagens
+ */
 class DataReceiver
 {
+  /**
+   * Instância da classe Database para a tabela tb_moviments
+   * @var Database
+   */
   private $mouse;
+
+  /**
+   * Instância da classe Database para a tabela tb_captures
+   * @var Database
+   */
   private $capture;
+
+  /**
+   * Construtor da classe
+   */
   public function __construct()
   {
+    // Inicializa as variáveis mouse e capture com instâncias da classe Database
     $this->mouse  = new Database("tb_moviments");
     $this->capture  = new Database("tb_captures");
   }
 
+  /**
+   * Função que recebe os dados da captura
+   */
   public function receiveData()
   {
+    // Recebe os dados em formato JSON e decodifica para um array
     $data = json_decode(file_get_contents("php://input"), true);
 
+    // Verifica se o tipo de dados está definido
     if (!isset($data["type"])) {
+      // Retorna um array vazio se o tipo não estiver definido
       echo json_encode([]);
       return;
     }
 
+    // Verifica se a URL já existe na tabela tb_captures
     $idUrl = $this->capture->select("url = '{$data["url"]}'")->fetch(PDO::FETCH_ASSOC);
+
+    // Se a URL não existe e o tipo de dados não é "image", retorna um array vazio
     if (!$idUrl && $data["type"] <> "image") {
       echo json_encode([]);
       return false;
-    } elseif ($idUrl && $data["type"] == "image") {
+    }
+    // Se a URL já existe e o tipo de dados é "image", retorna um array vazio
+    elseif ($idUrl && $data["type"] == "image") {
       echo json_encode([]);
       return false;
     }
 
+    // Verifica o tipo de dados e processa-os de acordo
     switch ($data["type"]) {
       case "data":
+        // Processa os dados de cliques e movimentos do mouse
         echo json_encode([
           $this->processClickData($data["clicks"], $idUrl["id"]),
           $this->processMoveData($data["movements"], $idUrl["id"])
         ]);
         break;
       case "image":
-        echo json_encode($this->processImageData($data["imageData"], $data["url"])??[]);
+        // Processa os dados de imagem
+        echo json_encode($this->processImageData($data["imageData"], $data["url"]) ?? []);
         break;
     }
   }
 
+  /**
+   * Função que processa os dados de cliques do mouse
+   * @param array $clicks Dados dos cliques do mouse
+   * @param int $url ID da URL na tabela tb_captures
+   * @return array IDs dos cliques inseridos na tabela tb_moviments
+   * 
+   * @return array IDs dos cliques inseridos na tabela tb_moviments
+   */
   public function processClickData($clicks, $url)
   {
     $ids = [];
@@ -53,13 +93,20 @@ class DataReceiver
         "url" => $url,
         "tipo_move" => "click",
         "ip_user" => $_SERVER["REMOTE_ADDR"],
-        "id_user" => $_SESSION["user"]["id"]??null
+        "id_user" => $_SESSION["user"]["id"] ?? null
       ];
       $ids[] = $this->mouse->insert($dataInsert);
     }
     return $ids;
   }
 
+  /**
+   * Função que processa os dados dos movimentos do mouse
+   * @param array $mouseMovements Dados dos movimentos do mouse
+   * @param int $url ID da URL na tabela tb_captures
+   * 
+   * @return array IDs dos movimentos inseridos na tabela tb_moviments
+   */
   public function processMoveData($mouseMovements, $url)
   {
     $ids = [];
@@ -72,35 +119,35 @@ class DataReceiver
         "url" => $url,
         "tipo_move" => "move",
         "ip_user" => $_SERVER["REMOTE_ADDR"],
-        "id_user" => $_SESSION["user"]["id"]??null
+        "id_user" => $_SESSION["user"]["id"] ?? null
       ];
       $ids[] = $this->mouse->insert($dataInsert);
     }
     return $ids;
   }
 
+  /**
+   * Função que processa os dados da imagem capturada
+   * @param string $imageData Dados da imagem codificados em base64
+   * @param string $url URL da página capturada
+   * 
+   * @return int|null ID da imagem inserida na tabela tb_captures ou null caso a dimensão da imagem já exista
+   */
   public function processImageData($imageData, $url)
   {
-    // decode the base64 encoded image data
     $image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $imageData));
 
-    // get the image dimensions
     $imageInfo = getimagesizefromstring($image);
     $imageWidth = $imageInfo[0];
     $imageHeight = $imageInfo[1];
 
-    // check if the image dimension already exists in the database
     $existingImage = $this->capture->select("url = '{$url}' AND dimensao = '{$imageWidth}x{$imageHeight}'")->fetch(PDO::FETCH_ASSOC);
 
-    // if the image dimension doesn't exist, insert it into the database
     if (!$existingImage) {
-      // generate a unique file name for the image
       $fileName = "uploads/" . uniqid() . '.jpeg';
 
-      // store the image on the server
       file_put_contents($fileName, $image);
 
-      // insert the image information into the database
       $dataInsert = [
         "url" => $url,
         "imagem" => $fileName,
